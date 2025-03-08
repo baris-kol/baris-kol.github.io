ced8703b63e5947cb0662284fb032748c2e8be0b
Barış KOL <baris.kol16@gmail.com>
2025-03-08 09:30:00 +0300

    canvaskit build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "255457985"
+    serviceWorkerVersion: "4098661821"
   }
 });
